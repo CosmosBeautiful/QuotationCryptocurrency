79bc89770334374c765599e53f5ef83b78268267
CosmosBeautiful <cosmos.beautiful@tutamail.com>
2019-09-30 10:18:48 +0500

    add Pagination

---
+++ QuotationCryptocurrency/QuotationCryptocurrency/TagHelpers/PageLinkTagHelper.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using QuotationCryptocurrency.Models;
+
+namespace QuotationCryptocurrency.TagHelpers
+{
+    public class PageLinkTagHelper : TagHelper
+    {
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public string PageAction { get; set; }
+        public PageNavigation PageNavigation { get; set; }
+
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory helperFactory)
+        {
+            urlHelperFactory = helperFactory;
+        }
+    
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            output.TagName = "nav";
+
+            TagBuilder tag = new TagBuilder("ul");
+            tag.AddCssClass("pagination");
+
+            TagBuilder currentItem = CreatePageItem(PageNavigation.PageNumber, urlHelper);
+            if (PageNavigation.HasPreviousPage)
+            {
+                TagBuilder prevItem = CreatePageItem(PageNavigation.PageNumber - 1, urlHelper);
+                tag.InnerHtml.AppendHtml(prevItem);
+            }
+
+            tag.InnerHtml.AppendHtml(currentItem);
+            if (PageNavigation.HasNextPage)
+            {
+                TagBuilder nextItem = CreatePageItem(PageNavigation.PageNumber + 1, urlHelper);
+                tag.InnerHtml.AppendHtml(nextItem);
+            }
+            output.Content.AppendHtml(tag);
+        }
+
+        TagBuilder CreatePageItem(int pageNumber, IUrlHelper urlHelper)
+        {
+            TagBuilder item = new TagBuilder("li");
+            item.AddCssClass("page-item");
+
+            TagBuilder link = new TagBuilder("a");
+            link.AddCssClass("page-link");
+
+            if (pageNumber == this.PageNavigation.PageNumber)
+            {
+                item.AddCssClass("active");
+            }
+            else
+            {
+                link.Attributes["href"] = urlHelper.Action(PageAction, new { pageData = pageNumber });
+            }
+            link.InnerHtml.Append(pageNumber.ToString());
+            item.InnerHtml.AppendHtml(link);
+            return item;
+        }
+    }
+}
