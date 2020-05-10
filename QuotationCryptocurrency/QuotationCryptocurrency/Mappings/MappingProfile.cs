d2930fd3595d1de40f8e89435ace7d1471dd44d6
CosmosIsBeautiful <cosmos.beautiful.work@gmail.com>
2020-05-10 14:28:34 +0500

    refactoring

--- QuotationCryptocurrency/QuotationCryptocurrency/Mappings/MappingProfile.cs
+++ QuotationCryptocurrency/QuotationCryptocurrency/Mappings/MappingProfile.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using QuotationCryptocurrency.Database.Models;
 using QuotationCryptocurrency.Models;
-using QuotationCryptocurrency.Request.Parameters;
 using QuotationCryptocurrency.Request.Parameters.CoinMarkerCap;
 
 namespace QuotationCryptocurrency.Mappings
@@ -9,6 +8,15 @@ namespace QuotationCryptocurrency.Mappings
     public class MappingProfile : Profile
     {
         public MappingProfile()
+        {
+            CreateMapQuotation();
+
+            CreateMapCrypto();
+
+            CreateMapQuote();
+        }
+
+        public void CreateMapQuotation()
         {
             CreateMap<Quotation, QuotationModel>()
                 .ForMember(x => x.Id, s => s.MapFrom(x => x.Id))
@@ -21,17 +29,20 @@ namespace QuotationCryptocurrency.Mappings
                 .ForMember(x => x.MarketCap, s => s.MapFrom(x => x.MarketCap))
                 .ForMember(x => x.LastUpdated, s => s.MapFrom(x => x.LastUpdated));
 
-            CreateMap<Quotation, QuotationModel>()
+            CreateMap<CoinMarkerCapParam, QuotationModel>()
                 .ForMember(x => x.Id, s => s.MapFrom(x => x.Id))
-                .ForMember(x => x.CryptoId, s => s.MapFrom(x => x.CryptoId))
+                .ForMember(x => x.CryptoId, s => s.MapFrom(x => x.Id))
                 .ForMember(x => x.Name, s => s.MapFrom(x => x.Name))
                 .ForMember(x => x.Symbol, s => s.MapFrom(x => x.Symbol))
-                .ForMember(x => x.Price, s => s.MapFrom(x => x.Price))
-                .ForMember(x => x.PercentChange1H, s => s.MapFrom(x => x.PercentChange1h))
-                .ForMember(x => x.PercentChange24H, s => s.MapFrom(x => x.PercentChange24h))
-                .ForMember(x => x.MarketCap, s => s.MapFrom(x => x.MarketCap))
-                .ForMember(x => x.LastUpdated, s => s.MapFrom(x => x.LastUpdated));
+                .ForMember(x => x.Price, s => s.MapFrom(x => x.Quote.Currency.Price))
+                .ForMember(x => x.PercentChange1H, s => s.MapFrom(x => x.Quote.Currency.PercentChange1h))
+                .ForMember(x => x.PercentChange24H, s => s.MapFrom(x => x.Quote.Currency.PercentChange24h))
+                .ForMember(x => x.MarketCap, s => s.MapFrom(x => x.Quote.Currency.MarketCap))
+                .ForMember(x => x.LastUpdated, s => s.MapFrom(x => x.Quote.Currency.LastUpdated));
+        }
 
+        public void CreateMapCrypto()
+        {
             CreateMap<Crypto, CryptoModel>()
                 .ForMember(x => x.Id, s => s.MapFrom(x => x.Id))
                 .ForMember(x => x.Name, s => s.MapFrom(x => x.Name))
@@ -41,7 +52,10 @@ namespace QuotationCryptocurrency.Mappings
                 .ForMember(x => x.Id, s => s.MapFrom(x => x.Id))
                 .ForMember(x => x.Name, s => s.MapFrom(x => x.Name))
                 .ForMember(x => x.Symbol, s => s.MapFrom(x => x.Symbol));
+        }
 
+        public void CreateMapQuote()
+        {
             CreateMap<Quote, QuoteModel>()
                 .ForMember(x => x.Id, s => s.MapFrom(x => x.Id))
                 .ForMember(x => x.Price, s => s.MapFrom(x => x.Price))
@@ -57,17 +71,6 @@ namespace QuotationCryptocurrency.Mappings
                 .ForMember(x => x.PercentChange24h, s => s.MapFrom(x => x.PercentChange24h))
                 .ForMember(x => x.MarketCap, s => s.MapFrom(x => x.MarketCap))
                 .ForMember(x => x.LastUpdated, s => s.MapFrom(x => x.LastUpdated));
-
-            CreateMap<CoinMarkerCapParam, QuotationModel>()
-                .ForMember(x => x.Id, s => s.MapFrom(x => x.Id))
-                .ForMember(x => x.CryptoId, s => s.MapFrom(x => x.Id))
-                .ForMember(x => x.Name, s => s.MapFrom(x => x.Name))
-                .ForMember(x => x.Symbol, s => s.MapFrom(x => x.Symbol))
-                .ForMember(x => x.Price, s => s.MapFrom(x => x.Quote.Currency.Price))
-                .ForMember(x => x.PercentChange1H, s => s.MapFrom(x => x.Quote.Currency.PercentChange1h))
-                .ForMember(x => x.PercentChange24H, s => s.MapFrom(x => x.Quote.Currency.PercentChange24h))
-                .ForMember(x => x.MarketCap, s => s.MapFrom(x => x.Quote.Currency.MarketCap))
-                .ForMember(x => x.LastUpdated, s => s.MapFrom(x => x.Quote.Currency.LastUpdated));
         }
     }
 }
